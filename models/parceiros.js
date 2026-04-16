ea5f573aa02e1ca8b7577cdc7f77b3ec61026686
Pedro Ignácio Molina Brião <pedrobriao.bg008@academico.ifsul.edu.br>
2026-04-16 09:03:30 -0300

    Atualizei algo ai

--- models/parceiros.js
+++ models/parceiros.js
@@ -16,25 +16,29 @@ const parceiros = {
             id: 1,
             nome: 'PROPESP',
             descricao: 'Pró-Reitoria de Pesquisa, Inovação e Pós-Graduação do IFSUL.',
-            website: 'https://ifsul.edu.br/propesp'
+            website: 'https://ifsul.edu.br/propesp',
+            logo: '/logos/propesp.png'
         },
         {
             id: 2,
             nome: 'FAPERGS',
             descricao: 'Fundação de Amparo à Pesquisa do Estado do Rio Grande do Sul.',
-            website: 'https://fapergs.rs.gov.br'
+            website: 'https://fapergs.rs.gov.br',
+            logo: '/logos/fapergs.png'
         },
         {
             id: 3,
             nome: 'CNPq',
             descricao: 'Conselho Nacional de Desenvolvimento Científico e Tecnológico.',
-            website: 'https://www.gov.br/cnpq'
+            website: 'https://www.gov.br/cnpq',
+            logo: '/logos/cnpq.png'
         },
         {
             id: 4,
             nome: 'CAPES',
             descricao: 'Coordenação de Aperfeiçoamento de Pessoal de Nível Superior.',
-            website: 'https://www.gov.br/capes'
+            website: 'https://www.gov.br/capes',
+            logo: '/logos/capes.png'
         }
     ]
 };
