eb60f7846342a9f51ae1e38df7102d94407faeee
Pedro Ignácio Molina Brião <pedrobriao.bg008@academico.ifsul.edu.br>
2026-04-15 21:45:48 -0300

    Atualizei algo ai

--- models/equipe.js
+++ models/equipe.js
@@ -1,84 +1,121 @@
+const secoes = [
+    {
+        id: 'professores-pesquisadores',
+        titulo: 'Professores e Pesquisadores',
+        tema: 'azul',
+        icone: '/icone olho rosa.png',
+        // Dica: coloque as imagens em public/equipe/<grupo>/ e ajuste o caminho abaixo.
+        integrantes: [
+            {
+                id: 1,
+                nome: 'Marcelo Siedler',
+                cargo: 'Docente do Curso de Computação',
+                instituicao: 'IFSul',
+                foto: '/equipe/professores-pesquisadores/marcelo-siedler.jpg',
+                bio: 'Coordenador do projeto +LUDUS'
+            },
+            {
+                id: 2,
+                nome: 'Rafael Cardoso',
+                cargo: 'Docente da Área de Computação',
+                instituicao: 'IFSul',
+                foto: '/equipe/professores-pesquisadores/rafael-cardoso.jpg',
+                bio: 'Pesquisador em Tecnologias Educacionais'
+            },
+            {
+                id: 3,
+                nome: 'Michelle Schmidt',
+                cargo: 'Pesquisadora em Educação Inclusiva',
+                instituicao: 'IFSul',
+                foto: '/equipe/professores-pesquisadores/michelle-schmidt.jpg',
+                bio: 'Especialista em Design Acessível para UTI'
+            }
+        ]
+    },
+    {
+        id: 'alunos-bolsistas',
+        titulo: 'Alunos e Bolsistas',
+        tema: 'rosa',
+        icone: '/icone equipe rosa.png',
+        integrantes: [
+            {
+                id: 4,
+                nome: 'Pedro Almeida',
+                cargo: 'Bolsista de Desenvolvimento',
+                instituicao: 'IFSul',
+                foto: '/equipe/alunos-bolsistas/pedro-almeida.jpg',
+                bio: 'Desenvolvedor de Jogos Educacionais'
+            },
+            {
+                id: 5,
+                nome: 'Julia Martins',
+                cargo: 'Bolsista de Design',
+                instituicao: 'IFSul',
+                foto: '/equipe/alunos-bolsistas/julia-martins.jpg',
+                bio: 'Designer UX/UI do Projeto'
+            },
+            {
+                id: 6,
+                nome: 'Lucas Siqueira',
+                cargo: 'Pesquisador - Bolsa CNPq',
+                instituicao: 'IFSul',
+                foto: '/equipe/alunos-bolsistas/lucas-siqueira.jpg',
+                bio: 'Análise de Acessibilidade'
+            },
+            {
+                id: 7,
+                nome: 'Mariana Lima',
+                cargo: 'Estagiária de Educação',
+                instituicao: 'Universidade Federal',
+                foto: '/equipe/alunos-bolsistas/mariana-lima.jpg',
+                bio: 'Metodologia Educacional'
+            }
+        ]
+    },
+    {
+        id: 'colaboradores-externos',
+        titulo: 'Colaboradores Externos',
+        tema: 'azul',
+        icone: '/icone informacao azul (2).png',
+        integrantes: [
+            {
+                id: 8,
+                nome: 'Marcelo Siedler',
+                cargo: 'Especialista em Acessibilidade',
+                instituicao: 'Consultoria para Inclusão Digital',
+                foto: '/equipe/colaboradores-externos/marcelo-siedler.jpg',
+                bio: 'Consultor WCAG 2.1 e GAIA'
+            },
+            {
+                id: 9,
+                nome: 'Rafael Cardoso',
+                cargo: 'Consultor de Tecnologia',
+                instituicao: 'Inovação & Tecnologia',
+                foto: '/equipe/colaboradores-externos/rafael-cardoso.jpg',
+                bio: 'Especialista em Arquitetura de Software'
+            },
+            {
+                id: 10,
+                nome: 'Michelle Schmidt',
+                cargo: 'Consultora Psicopedagógica',
+                instituicao: 'Instituto de Educação Inclusiva',
+                foto: '/equipe/colaboradores-externos/michelle-schmidt.jpg',
+                bio: 'Metodologia para Educação Especial'
+            }
+        ]
+    }
+];
+
 const equipe = {
     descricao: 'Uma equipe multidisciplinar dedicada a criar soluções inclusivas e inovadoras em educação digital',
-    
-    professoresePesquisadores: [
-        {
-            id: 1,
-            nome: 'Marcelo Siedler',
-            cargo: 'Docente do Curso de Computação',
-            instituicao: 'IFSul',
-            bio: 'Coordenador do projeto +LUDUS'
-        },
-        {
-            id: 2,
-            nome: 'Rafael Cardoso',
-            cargo: 'Docente da Área de Computação',
-            instituicao: 'IFSul',
-            bio: 'Pesquisador em Tecnologias Educacionais'
-        },
-        {
-            id: 3,
-            nome: 'Michelle Schmidt',
-            cargo: 'Pesquisadora em Educação Inclusiva',
-            instituicao: 'IFSul',
-            bio: 'Especialista em Design Acessível para UTI'
-        }
-    ],
-    
-    alunoseBolsistas: [
-        {
-            id: 4,
-            nome: 'Pedro Almeida',
-            cargo: 'Bolsista de Desenvolvimento',
-            instituicao: 'IFSul',
-            bio: 'Desenvolvedor de Jogos Educacionais'
-        },
-        {
-            id: 5,
-            nome: 'Julia Martins',
-            cargo: 'Bolsista de Design',
-            instituicao: 'IFSul',
-            bio: 'Designer UX/UI do Projeto'
-        },
-        {
-            id: 6,
-            nome: 'Lucas Siqueira',
-            cargo: 'Pesquisador - Bolsa CNPq',
-            instituicao: 'IFSul',
-            bio: 'Análise de Acessibilidade'
-        },
-        {
-            id: 7,
-            nome: 'Mariana Lima',
-            cargo: 'Estagiária de Educação',
-            instituicao: 'Universidade Federal',
-            bio: 'Metodologia Educacional'
-        }
-    ],
-    
-    colaboradoresExternos: [
-        {
-            id: 8,
-            nome: 'Marcelo Siedler',
-            cargo: 'Especialista em Acessibilidade',
-            instituicao: 'Consultoria para Inclusão Digital',
-            bio: 'Consultor WCAG 2.1 e GAIA'
-        },
-        {
-            id: 9,
-            nome: 'Rafael Cardoso',
-            cargo: 'Consultor de Tecnologia',
-            instituicao: 'Inovação & Tecnologia',
-            bio: 'Especialista em Arquitetura de Software'
-        },
-        {
-            id: 10,
-            nome: 'Michelle Schmidt',
-            cargo: 'Consultora Psicopedagógica',
-            instituicao: 'Instituto de Educação Inclusiva',
-            bio: 'Metodologia para Educação Especial'
-        }
-    ]
+
+    // Edite membros e títulos diretamente neste array para atualizar toda a seção de equipe.
+    secoes,
+
+    // Mantidos por compatibilidade com estruturas antigas do projeto.
+    professoresePesquisadores: secoes[0].integrantes,
+    alunoseBolsistas: secoes[1].integrantes,
+    colaboradoresExternos: secoes[2].integrantes
 };
 
 module.exports = equipe;
---
+++ public/equipe/README.md
@@ -0,0 +1,32 @@
+# Imagens da Equipe
+
+Coloque aqui as fotos dos integrantes da pagina de equipe.
+
+Estrutura recomendada:
+
+- professores-pesquisadores/
+- alunos-bolsistas/
+- colaboradores-externos/
+
+Nomes esperados atualmente no modelo:
+
+## professores-pesquisadores
+
+- marcelo-siedler.jpg
+- rafael-cardoso.jpg
+- michelle-schmidt.jpg
+
+## alunos-bolsistas
+
+- pedro-almeida.jpg
+- julia-martins.jpg
+- lucas-siqueira.jpg
+- mariana-lima.jpg
+
+## colaboradores-externos
+
+- marcelo-siedler.jpg
+- rafael-cardoso.jpg
+- michelle-schmidt.jpg
+
+Se uma foto nao existir, o site mostra automaticamente o avatar colorido.
